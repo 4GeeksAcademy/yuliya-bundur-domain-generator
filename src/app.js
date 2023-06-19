67178f1770fbec77ec964ae584bd28563ae54268
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-06-19 10:03:45 +0000

    Mi generador de dominios

--- src/app.js
+++ src/app.js
@@ -2,10 +2,34 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var domainGenerate = {
+    pronoun: ["the", "our", "your"],
+    adj: ["great", "big", "best", "first", "perfect"],
+    noun: ["place", "corner", "experience", "vacation"],
+    topLavelDomain: [".com", ".es", ".by", ".de", ".org", ".fr", ".edu"]
+  };
+  console.log(domainGenerate);
+
+  var firstParraf = document.getElementById("firstParraf");
+  var generateButton = document.getElementById("generateButton");
+  var myDomain = document.getElementById("myDomain");
+
+  generateButton.addEventListener("click", function() {
+    var rundomPronoun = Math.floor(
+      Math.random() * domainGenerate.pronoun.length
+    );
+    var rundomAdj = Math.floor(Math.random() * domainGenerate.adj.length);
+    var rundomNoun = Math.floor(Math.random() * domainGenerate.noun.length);
+    var rundomTopLavelDomain = Math.floor(
+      Math.random() * domainGenerate.topLavelDomain.length
+    );
+
+    var result = `${domainGenerate.pronoun[rundomPronoun]}${domainGenerate.adj[rundomAdj]}${domainGenerate.noun[rundomNoun]}${domainGenerate.topLavelDomain[rundomTopLavelDomain]}`;
+    myDomain.innerHTML = result;
+  });
 };
